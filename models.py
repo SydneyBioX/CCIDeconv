0ad689a8c1c2719c76bd0463c3d9faf4d9791a47
Rojashree Jayakumar <rjay0256@uni.sydney.edu.au>
2026-02-09 19:38:11 +1100

    updated comments

--- models.py
+++ models.py
@@ -3,12 +3,24 @@ from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from xgboost import XGBClassifier, XGBRegressor
 
 class ClassifierWrapper:
+    """
+        Wrapper for a classification model using RandomForest and XGBClassifier in a soft VotingClassifier.
+        Args:
+        rf_params (dict, optional): Parameters for RandomForestClassifier.
+        xgb_params (dict, optional): Parameters for XGBClassifier.
+    
+    Attributes:
+        model (VotingClassifier): The trained voting classifier combining RF and XGB.
+    """
     def __init__(self, rf_params=None, xgb_params=None):
         self.rf_params = rf_params
         self.xgb_params = xgb_params
         self.model = None
 
     def train(self, X_train, y_train):
+        """
+        Train the voting classifier on the given data.
+        """
         rf = RandomForestClassifier(**self.rf_params)
         xgb = XGBClassifier(**self.xgb_params,
                             nthread = -1,
@@ -19,11 +31,23 @@ class ClassifierWrapper:
         return self.model
 
 class RegressorWrapper:
+    """
+    Wrapper for an XGBoost regression model with optional early stopping.
+
+    Args:
+        xgb_params (dict, optional): Parameters for XGBRegressor.
+
+    Attributes:
+        model (XGBRegressor): The trained XGBoost regressor.
+    """
     def __init__(self, xgb_params):
         self.xgb_params = xgb_params
         self.model = None
 
     def train(self, X_train, y_train, X_val=None, y_val=None):
+        """
+        Train the  regressor on the given data.
+        """
         params = dict(self.xgb_params)
         params.update({
             "objective": "reg:squarederror",
